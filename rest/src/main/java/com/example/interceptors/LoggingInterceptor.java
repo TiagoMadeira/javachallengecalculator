8eaa91b11191c29b8528be3bb498e307143b41eb
TiagoMadeira <tiagopmadeira@gmail.com>
2024-12-19 20:49:27 +0000

    Update log levels on API response logs

--- rest/src/main/java/com/example/interceptors/LoggingInterceptor.java
+++ rest/src/main/java/com/example/interceptors/LoggingInterceptor.java
@@ -1,11 +1,13 @@
 package com.example.interceptors;
 
+import com.example.exceptions.OperationDoesNotExistException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -28,14 +30,22 @@ public class LoggingInterceptor implements HandlerInterceptor{
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+
+        int status = response.getStatus();
+        String message = "[Rest][Request][Output] resolved!";
+
         //Add info to MDC
         MDC.put("Response.status", String.valueOf(response.getStatus()));
 
         //Append MDC request.id to the response header
         response.addHeader("Request.id", MDC.get("Request.id"));
 
-        logger.info("[Rest][Request][Output] resolved!");
-
+        if (status == HttpStatus.BAD_REQUEST.value() || status == HttpStatus.INTERNAL_SERVER_ERROR.value()){
+            logger.warn(message);
+        }
+        else {
+            logger.info(message);
+        }
         //Clear the MDC
         MDC.clear();
     }
